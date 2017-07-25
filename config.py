186528b16b3c0695ea3efdcd5d23e266193b41e3
justmesam <sthiongo12@gmail.com>
2017-07-25 23:45:15 +0300

    [CHORE] Add the api configurations

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG=False
+    SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+
+class DevelopmentConfig(Config):
+    DEBUG=True
