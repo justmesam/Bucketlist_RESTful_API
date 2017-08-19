18dcd25c4a4796a4d290ef726579fd97fa82f163
justmesam <samuel.wanjiru@andela.com>
2017-08-19 22:30:06 +0300

    [UPDATE] Register the bucketlist blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,5 +8,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_ = SQLAlchemy()
 db_.init_app(app)
 
+
 from app.auth import auth_blueprint
-app.register_blueprint(auth_blueprint)
+from app.bucketlist import bucketlist_blueprint
+app.register_blueprint(auth_blueprint, url_prefix='/api/v1.0/auth')
+app.register_blueprint(bucketlist_blueprint, url_prefix='/api/v1.0/bucketlists')
