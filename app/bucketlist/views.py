93622dd389f53979ba8f705dfce66b0f05fb3594
justmesam <samuel.wanjiru@andela.com>
2017-08-24 19:20:37 +0300

    [FEATURE] Add the pagination feature

--- app/bucketlist/views.py
+++ app/bucketlist/views.py
@@ -1,5 +1,6 @@
-from flask import make_response, request, jsonify
+from flask import make_response, request, jsonify, url_for
 from flask.views import MethodView
+from app.common import auth_required
 from app.models import User, Bucketlist
 from app.bucketlist import bucketlist_blueprint
 
@@ -8,109 +9,25 @@ class BucketlistApi(MethodView):
     """
     The class bucketlist with all the methods
     """
-    def get(self, _id=None):
+    @auth_required
+    def get(self, auth_data, _id=None):
         """
         method gets all bucketlist for a user
         """
-        auth_header = request.headers.get('Authorization')
-        auth_token = auth_header.split("Bearer ")[1]
-        if auth_token:
-            auth_data = User.token_decoding(auth_token)
-            if not isinstance(auth_data, str):
-                if not _id:
-                    bucketlists = Bucketlist.query_all(auth_data)
-                    if not bucketlists:
-                        response = {
-                            'message' : 'No bucketlists for this user yet'
-                        }
-                        return make_response(jsonify(response)), 404
-                    else:
-                        all_bucketlists = []
-                        for bucketlist in bucketlists:
-                            obj = {
-                                'id' : bucketlist.id,
-                                'owner' : bucketlist.owner,
-                                'date_created' : bucketlist.date_created,
-                                'date_updated' : bucketlist.date_updated,
-                                'title' : bucketlist.title,
-                                'intro' : bucketlist.intro
-                            }
-                            all_bucketlists.append(obj)
-                            return make_response(jsonify(all_bucketlists)), 200
-                else:
-                    bucketlist = Bucketlist.query.filter_by(id=_id).first()
-                    if not bucketlist:
-                        response = {
-                            'message' : 'Bucketlist not available'
-                        }
-                        return make_response(jsonify(response)), 404
-                    else:
-                        response = {
-                            'id' : bucketlist.id,
-                            'owner' : bucketlist.owner,
-                            'date_created' : bucketlist.date_created,
-                            'date_updated' : bucketlist.date_updated,
-                            'title' : bucketlist.title,
-                            'intro' : bucketlist.intro
-                        }
-                        return make_response(jsonify(response)), 200
-            else:
-                response = {
-                    'message' : auth_data
-                }
-                return make_response(jsonify(response)), 401
-        else:
-            response = {
-                'message' : 'No valid token'
-            }
-            return make_response(jsonify(response)), 401
-
-
-    def post(self):
-        """
-        method used to create a database
-        """
-        auth_header = request.headers.get('Authorization')
-        auth_token = auth_header.split("Bearer ")[1]
-        if auth_token:
-            auth_data = User.token_decoding(auth_token)
-            if not isinstance(auth_data, str):
-                title = request.get_json().get('title')
-                intro = request.get_json().get('intro')
-                if intro and title:
-                    bucketlist = Bucketlist(title=title,
-                                            intro=intro,
-                                            owner=auth_data)
-                    bucketlist.save_bucketlist()
+        limit = request.args.get('limit')
+        page = request.args.get('page')
+        if not limit:
+            if not _id:
+                bucketlists = Bucketlist.query_all(auth_data)
+                if not bucketlists:
                     response = {
-                        'id' : bucketlist.id,
-                        'owner' : bucketlist.owner,
-                        'date_created' : bucketlist.date_created,
-                        'date_updated' : bucketlist.date_updated,
-                        'title' : bucketlist.title,
-                        'intro' : bucketlist.intro
+                        'message' : 'No bucketlists for this user yet'
                     }
-                    return make_response(jsonify(response)), 201
+                    return make_response(jsonify(response)), 404
+                else:
+                    all_bucketlists = [bucketlist.serialize() for bucketlist in bucketlists]
+                    return make_response(jsonify(all_bucketlists)), 200
             else:
-                response = {
-                    'message' : auth_data
-                }
-                return make_response(jsonify(response)), 401
-        else:
-            response = {
-                'message' : 'No valid token'
-            }
-            return make_response(jsonify(response)), 401
-
-    def put(self, _id):
-        """
-        method used to update and delete a specific DATABASE using its id
-        """
-        auth_header = request.headers.get('Authorization')
-        auth_token = auth_header.split("Bearer ")[1]
-        if auth_token:
-            auth_data = User.token_decoding(auth_token)
-            if not isinstance(auth_data, str):
                 bucketlist = Bucketlist.query.filter_by(id=_id).first()
                 if not bucketlist:
                     response = {
@@ -118,62 +35,96 @@ class BucketlistApi(MethodView):
                     }
                     return make_response(jsonify(response)), 404
                 else:
-                    title = request.get_json().get('title')
-                    intro = request.get_json().get('intro')
-                    bucketlist.title = title
-                    bucketlist.intro = intro
-                    bucketlist.save_bucketlist()
-                    response = {
-                        'id' : bucketlist.id,
-                        'owner' : bucketlist.owner,
-                        'date_created' : bucketlist.date_created,
-                        'date_updated' : bucketlist.date_updated,
-                        'title' : bucketlist.title,
-                        'intro' : bucketlist.intro
-                    }
+                    response = bucketlist.serialize()
                     return make_response(jsonify(response)), 200
+        else:
+            try:
+                limit = int(limit)
+                page = int(page)
+            except ValueError:
+                limit = 5
+                page = 1
+            limited_bucketlists = Bucketlist.query.filter_by\
+             (owner=auth_data).paginate(page, limit, False)
+            all_bucketlists = [bucketlist.serialize() for bucketlist in limited_bucketlists.items]
+            if all_bucketlists:
+                next_page = ''
+                previous_page = ''
+                def url_for_other_page(page):
+                    """
+                    generates url for the next and prev page of pagination
+                    """
+                    args = request.view_args.copy()
+                    args['page'] = page
+                    return url_for(request.endpoint, **args)
+                if limited_bucketlists.has_next:
+                    next_page = 'http://127.0.0.1:5000/' +\
+                     url_for_other_page(page + 1)  + '&limit' + str(limit)
+                if limited_bucketlists.has_prev:
+                    previous_page = 'http://127.0.0.1:5000/' +\
+                     url_for_other_page(page - 1)  + '&limit' + str(limit)
             else:
                 response = {
-                    'message' : auth_data
+                    'message' : 'No bucketlits available'
                 }
-                return make_response(jsonify(response)), 401
-        else:
-            response = {
-                'message' : 'No valid token'
-            }
-            return make_response(jsonify(response)), 401
+                return make_response(jsonify(response)), 404
+            return make_response(jsonify(bucketlists=all_bucketlists,
+                                         next_page=next_page,
+                                         previous_page=previous_page)), 200
 
+    @auth_required
+    def post(self, auth_data):
+        """
+        method used to create a bucketlist
+        """
+        title = request.get_json().get('title')
+        intro = request.get_json().get('intro')
+        bucketlist_ = Bucketlist.query.filter_by(title=title).first()
+        if intro and title:
+            bucketlist = Bucketlist(title=title,
+                                    intro=intro,
+                                    owner=auth_data)
+            bucketlist.save_bucketlist()
+            response = bucketlist.serialize()
+            return make_response(jsonify(response)), 201
+
+    @auth_required
+    def put(self, _id, auth_data):
+        """
+        method used to update and delete a specific bucketlist using its id
+        """
+        bucketlist = Bucketlist.query.filter_by(id=_id).first()
+        if not bucketlist:
+            response = {
+                'message' : 'Bucketlist not available'
+            }
+            return make_response(jsonify(response)), 404
+        else:
+            title = request.get_json().get('title')
+            intro = request.get_json().get('intro')
+            bucketlist.title = title
+            bucketlist.intro = intro
+            bucketlist.save_bucketlist()
+            response = bucketlist.serialize()
+            return make_response(jsonify(response)), 200
+
+    @auth_required
     def delete(self, _id):
         """
         method used to delete a database using its id
         """
-        auth_header = request.headers.get('Authorization')
-        auth_token = auth_header.split("Bearer ")[1]
-        if auth_token:
-            auth_data = User.token_decoding(auth_token)
-            if not isinstance(auth_data, str):
-                bucketlist = Bucketlist.query.filter_by(id=_id).first()
-                if not bucketlist:
-                    response = {
-                        'message' : 'Bucketlist not available'
-                    }
-                    return make_response(jsonify(response)), 404
-                else:
-                    bucketlist.delete()
-                    response = {
-                        'message' : 'Bucketlist deleted'
-                    }
-                    return make_response(jsonify(response)), 200
-            else:
-                response = {
-                    'message' : auth_data
-                }
-                return make_response(jsonify(response)), 401
-        else:
+        bucketlist = Bucketlist.query.filter_by(id=_id).first()
+        if not bucketlist:
             response = {
-                'message' : 'No valid token'
+                'message' : 'Bucketlist not available'
             }
-            return make_response(jsonify(response)), 401
+            return make_response(jsonify(response)), 404
+        else:
+            bucketlist.delete()
+            response = {
+                'message' : 'Bucketlist deleted'
+            }
+            return make_response(jsonify(response)), 200
 
 
 bucket_view = BucketlistApi.as_view('bucketlist_api')
@@ -184,6 +135,6 @@ bucketlist_blueprint.add_url_rule('/',
 bucketlist_blueprint.add_url_rule('/',
                                   view_func=bucket_view,
                                   methods=['POST'])
-bucketlist_blueprint.add_url_rule('/<int:_id>',
+bucketlist_blueprint.add_url_rule('/<int:_id>/',
                                   view_func=bucket_view,
                                   methods=['GET', 'PUT', 'DELETE'])
