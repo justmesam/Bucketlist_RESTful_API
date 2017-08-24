b2cc016f6ee29d0c750ecde7a03f944240814ab9
justmesam <samuel.wanjiru@andela.com>
2017-08-24 19:09:19 +0300

    [FEATURE] Add the search endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+from functools import wraps
 from flask import Flask, make_response, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,45 +13,31 @@ from app.auth import auth_blueprint
 from app.bucketlist import bucketlist_blueprint
 from app.item import item_blueprint
 from app.models import Bucketlist, Item, User
+from app.common import auth_required
 
 
 @app.route('/search/', methods=['GET'])
-def search_api():
-    auth_header = request.headers.get('Authorization')
-    auth_token = auth_header.split("Bearer ")[1]
-    if auth_token:
-        auth_data = User.token_decoding(auth_token)
-        if not isinstance(auth_data, str):
-            search = request.args.get('q', type=str)
-            if not search:
-                response = {
-                    'message' : 'Enter a search parameter'
-                }
-                return make_response(jsonify(response)), 404
-            else:
-                bucketlist_found = Bucketlist.query.filter_by(owner=auth_data).\
-                 filter(Bucketlist.title.contains(search)).all()
-                all_bucketlists = []
-                for bucketlist in bucketlist_found:
-                    items = Item.query_items(bucketlist.id)
-                    all_items = []
-                    for item in items:
-                        obj = item.serialize()
-                        all_items.append(obj)
-                        bucketlist_ = bucketlist.serialize()
-                        bucketlist_['items'] = all_items
-                    all_bucketlists.append(bucketlist_)
-                return make_response(jsonify(bucketlists=all_bucketlists)), 200
-        else:
-            response = {
-                'message' : auth_data
-            }
-            return make_response(jsonify(response)), 401
-    else:
+@auth_required
+def search_api(auth_data, *args, **kwargs):
+    """
+    method for searching bucketlist by name/title
+    """
+    search = request.args.get('q', type=str)
+    if not search:
         response = {
-            'message' : 'No valid token'
+            'message' : 'Enter a search parameter'
         }
-        return make_response(jsonify(response)), 401
+        return make_response(jsonify(response)), 404
+    else:
+        bucketlist_found = Bucketlist.query.filter_by(owner=auth_data).\
+         filter(Bucketlist.title.contains(search)).all()
+        all_bucketlists = [bucketlist.serialize() for bucketlist in bucketlist_found]
+        for bucketlist in all_bucketlists:
+            bucketlist.update(
+                {"items" : [item.serialize()\
+                 for item in Item.query_items(bucketlist['id'])]}
+            )
+        return make_response(jsonify(bucketlists=all_bucketlists)), 200
 
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(bucketlist_blueprint, url_prefix='/bucketlists')
